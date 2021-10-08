b67de60646f43cfd45378ca040d20e2603ff2323
Eldow <66029651+Eldow227@users.noreply.github.com>
2021-10-08 14:37:37 +0200

    Add files via upload

--- DEMO_trading_program.py
+++ DEMO_trading_program.py
@@ -1,15 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Sep 26 10:58:52 2021
-
-@author: kmate
-"""
-
-# %%
-##TWORZYMY WYKRES O CENACH AKCJI (30-D)
 import random
 import matplotlib.pyplot as plt
 import find_tops
+import find_bottom
 values= []
 days = [i for i in range(1, 31)]
 while len(values) < 30:
@@ -26,14 +18,15 @@ ax.set_title('TEST PLOT')
 COORDINATES = line.get_xydata()
 print('Done!')
 # %%
-second = {}
+x_to_y_dict_2 = {}
 x_to_y_dict = {}
 for i in COORDINATES:
     x_to_y_dict[i[0]] = i[1]
 for i in COORDINATES:
-    second[i[0]] = i[1]
+    x_to_y_dict_2[i[0]] = i[1]
 
 print(x_to_y_dict)
+print(x_to_y_dict_2)
 # %%
 ##ZNAJDUJE X DLA DANEGO Y
 
@@ -57,10 +50,27 @@ tops_coordinates.append(x_list)
 print('Done!')
 print(tops_coordinates)
 # %%
-print('*'*10)
-print(y_list)
-print(x_list)
-print('*'*10)
+def show_x_2(y_value):
+    """Give y value to see its x."""
+    for key, value in x_to_y_dict_2.items():
+        if y_value == value:
+            del x_to_y_dict_2[key]
+            return key
+
+bottom_coordinates = []
+x_list2 = []
+y_list2 = []
+for y in find_bottom.bottom(values):
+    y_list2.append(y)
+    x_list2.append(show_x_2(y))
+
+bottom_coordinates.append(y_list2)
+bottom_coordinates.append(x_list2)
+print('Done!')
+print(bottom_coordinates)
+
+
+# %%
 print('Checking if correct...')
 anomaly = []
 indexes = []
@@ -78,6 +88,37 @@ for i in indexes:
 print('*'*10)
 print(y_list)
 print(x_list)
+print('*'*10)
+print(y_list2)
+print(x_list2)
+print('*'*10)
+
+# %%
+
+anomaly2 = []
+indexes2 = []
+print(x_list2)
+for i in range(1, len(x_list2)):
+    if x_list2[i] < x_list2[i-1]:
+        x_list2.remove(x_list2[i])
+        y_list2.remove(y_list2[i])
+        anomaly2.append(i)
+        
+# for i in anomaly2:
+#         x_list2.remove(i)
+
+# for i in indexes:
+#     y_list2.remove(y_list2[i])
+    
+print(len(x_list2))
+print(len(y_list2))
+
+
+    
+        
+    
+
+
 # %%
 # print(x_to_y_dict)
 # cure = []
@@ -101,7 +142,7 @@ print(x_list)
 ##RYSUJ NOWY WYKRES Z ZAZNACZONYMI SZCZYTAMI
 print(tops_coordinates)
 fig, ax = plt.subplots()
-plt.plot([day for day in days], [value for value in values], tops_coordinates[1], tops_coordinates[0])
+plt.plot([day for day in days], [value for value in values], tops_coordinates[1], tops_coordinates[0], bottom_coordinates[1], bottom_coordinates[0])
 plt.axis([0, 30, 0, 100])
 plt.show()
 line = ax.lines[0]
